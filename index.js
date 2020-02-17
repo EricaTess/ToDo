1b93fbdc2c15e4a9324455fda149b208c1a1b869
Erica Sanchez <ericasanchez@eri-air.lan>
2020-02-16 18:47:13 -0800

    Storing these just incase

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+//connecting to MongoDB Atlas
+//connect to the database
+const uri = 'mongodb+srv://eri-cat:monsters@cluster0-jj5ox.gcp.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } );
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully")
+})
+  .catch(err => console.log(err));
+
+// mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,8 +28,15 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Welcome to Express');
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
 });
 
 app.listen(port, () => {
--- models/todo.js
+++ models/todo.js
@@ -1,34 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Todo = require('../models/todo');
-
-router.get('/todos', (req, res, next) => {
-
-  //this will return all the data, exposing only the id and action field to the client
-  Todo.find({}, 'action')
-    .then(data => res.json(data))
-    .catch(next)
-});
-
-router.post('/todos', (req, res, next) => {
-  if(req.body.action){
-    Todo.create(req.body)
-      .then(data => res.json(data))
-      .catch(next)
-  } else {
-    res.json({
-      error: "the input field is empty"
-    })
-  }
-});
-
-router.delete('/todos/:id', (req, res, next) => {
-  Todo.findOneAndDelete({"_id": req.params.id})
-    .then(data => res.json(data))
-    .catch(next)
-})
-
-module.exports = router;
 
 const TodoSchema = new Schema({
   action: {
